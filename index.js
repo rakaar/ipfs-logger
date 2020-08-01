860a9f9ea108db58eb22b2f1062e4b17dd3b04e5
rakaar <rka87338@gmail.com>
2020-08-01 21:07:34 +0530

    Added POST and GET endpoints

--- index.js
+++ index.js
@@ -4,48 +4,108 @@ const IPFS = require('ipfs')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-async function main() {
-	let  cid;
-       // adding to ipfs
-       console.log("test called")
-       const node = await IPFS.create()
-       const version = await node.version()
-       
-       let  data = {
-        "activity" : "access data",
-	"by" : "me",
-	"to" : "me",
-	"time" : "now"
-      }
-      let stringified_data = JSON.stringify(data)
-      const  results =  node.add(stringified_data)
-
-     results.then(async res => { 
-     console.log("Res is ",res); 
-     cid = res.path 
-     console.log("cid is ", cid)
-     const stream = node.cat(cid);
-    console.log("steam ", stream)
-    let data1 = ``
-    for await (const chunk of stream) {
-  // chunks of data are returned as a Buffer, convert it back to a string
-     data1 += chunk.toString()
+const sqlite3 = require('sqlite3').verbose();
+const db_name =  "hashes.db";
+const db = new sqlite3.Database(db_name, err => {
+  if (err) {
+    return console.error(err.message);
   }
+  console.log("Successful connection to the Database");
+});
 
-   console.log(data1)
-}).catch(err => console.log("err is ",err))
-	
-      
+const sql_create = `CREATE TABLE IF NOT EXISTS Hashes (
+   Hash VARCHAR(300)
+ );`;
 
-}
+
+ db.run(sql_create, err => {
+   if (err) {
+     return console.error(err.message);
+   }
+   console.log("Successful creation of the 'Hashes' table");
+ });
+
+
+// GET endpoint to test if server is working or not
+app.get('/', (req, res) => res.send('Alive !'))
+
+
+//POST endpoint to add data to the IPFS
+app.post('/write', async (req, res) => {
+   const data = JSON.stringify(res.body)
+
+   const node = await IPFS.create()
+   const  results =  node.add(data)
+
+   results
+      .then(res => {
+        const sql_insert = `INSERT INTO Hashes (Hash) VALUES ${(res.path)};`;
+        db.run(sql_insert, err => {
+         if (err) {
+            return console.error(err.message);
+          }
+
+          console.log("Successful creation of Hashes");
+          res.send("success")
+
+        })
+
+      })
+      .catch(err => {
+         res.send(err);
+         return
+      })
+})
+
+// GET endpoint to get all the hashes and information from the hashes
+app.get('/read', (req, res) => {
+   const sql_read = `SELECT * FROM Hashes;`
+   db.all(sql_read, [], (err, rows) => {
+      if(err) {
+         console.log("Error in reading DB")
+         return err;
+      }
+
+      res.send(rows)
+
+   })
+})
+
+
+// async function main() {
+// 	let  cid;
+//        // adding to ipfs
+//        console.log("test called")
+//        const node = await IPFS.create()
+//        const version = await node.version()
+       
+//        let  data = {
+//         "activity" : "access data",
+//             	"by" : "me",
+// 	             "to" : "me",
+// 	            "time" : "now"
+//            }
+//       let stringified_data = JSON.stringify(data)
+//       const  results =  node.add(stringified_data)
+
+//      results.then(async res => { 
+//      console.log("Res is ",res); 
+//      cid = res.path 
+//      console.log("cid is ", cid)
+//      const stream = node.cat(cid);
+//     console.log("steam ", stream)
+//     let data1 = ``
+//     for await (const chunk of stream) {
+//   // chunks of data are returned as a Buffer, convert it back to a string
+//      data1 += chunk.toString()
+//   }
+
+//    console.log(data1)
+// }).catch(err => console.log("err is ",err))
+// }
 
 app.listen(port, () =>{
-
-main()
-
-
+   console.log(`Listening at port ${port}`)
 })
 
 
